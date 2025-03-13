6463bb89d44614d36fd4b577519319a1e300a148
Icein004 <aicinmp@gmail.com>
2025-03-13 10:33:52 +0530

    Arithmeticopreator

---
+++ arithmeticoperator/ArithmeticOperator.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperator{
+    public static void main(String srgs[]){
+        System.out.println(10+2);
+        System.out.println(10-2);
+        System.out.println(10*2);
+        System.out.println(10/2);
+        System.out.println(10%2);
+    }
+
+}
