f937428c1f17ecb0890d3314f9749b751d1fb333
Icein004 <aicinmp@gmail.com>
2025-03-13 10:41:41 +0530

    Conditinal

---
+++ conditionaloperator/Conditional.java
@@ -0,0 +1,10 @@
+public class Conditional{
+    public static void main(String srgs[]){
+        System.out.println(10>2);
+        System.out.println(10<2);
+        System.out.println(10>=2);
+        System.out.println(10<=11);
+        System.out.println(10==2);
+    }
+
+}
