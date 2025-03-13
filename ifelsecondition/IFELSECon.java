b234693212ba79d332c1a3630fcff2c82f5338e5
Icein004 <aicinmp@gmail.com>
2025-03-13 11:13:19 +0530

    ifelsecondtion

---
+++ ifelsecondition/IFELSECon.java
@@ -0,0 +1,11 @@
+public class IFELSECon{
+    public static void main(String args[]){
+        int no = 6;
+        if(no%2==0){
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is even");
+    }
+}
+}
